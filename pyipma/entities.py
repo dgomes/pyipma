7b988515da0cb359a762def103075d8575c7ee13
Diogo Gomes <dgomes@av.it.pt>
2019-11-24 20:04:28 +0000

    pylint

--- pyipma/entities.py
+++ pyipma/entities.py
@@ -3,22 +3,24 @@ from collections import UserDict, namedtuple
 
 from .consts import API_DISTRITS, API_WEATHER_TYPE, API_WIND_SPEED_DAILY, API_PRECIPITATION
 
-class Entities(UserDict):
+class Entities(UserDict): # pylint: disable=too-many-ancestors
+    """Generic base class for IPMA auxiliar services."""
     def __init__(self, entity_name, data, key):
+        super().__init__()
         self._data = {}
 
         if len(data) < 1:
-            raise Exception(f"No {self.__class__.name} data")
-        Data = namedtuple(entity_name, list(data["data"][0].keys()))
+            raise Exception(f"No {self.__class__.__name__} data")
+        data_type = namedtuple(entity_name, list(data["data"][0].keys()))
         for entry in data["data"]:
-            self._data[entry[key]] = Data._make(entry.values())
+            self._data[entry[key]] = data_type._make(entry.values())
 
     def __getitem__(self, key):
         return self._data[key]
 
     def __iter__(self):
         return iter(self._data)
-    
+
     def __len__(self):
         return len(self._data)
 
@@ -28,45 +30,50 @@ class Entities(UserDict):
     def values(self):
         return self._data.values()
 
+    def get(self, api): # pylint: disable=arguments-differ
+        raise NotImplementedError()
 
-class Distrit_Island(Entities):
+
+class DistritIsland(Entities): # pylint: disable=too-many-ancestors
+    """Object based representation of IPMA open API district_islands.json."""
     def __init__(self, data):
         super().__init__(self.__class__.__name__, data, 'globalIdLocal')
-    
+
     @classmethod
     async def get(cls, api):
         raw = await api.retrieve(API_DISTRITS)
 
         return cls(raw)
 
-class Weather_Type(Entities):
+class WeatherType(Entities): # pylint: disable=too-many-ancestors
+    """Object based representation of IPMA open API weather-type-classe.json."""
     def __init__(self, data):
         super().__init__(self.__class__.__name__, data, 'idWeatherType')
-    
+
     @classmethod
     async def get(cls, api):
         raw = await api.retrieve(API_WEATHER_TYPE)
 
         return cls(raw)
 
-class Wind_Speed_Daily(Entities):
+class WindSpeedDaily(Entities): # pylint: disable=too-many-ancestors
+    """Object based representation of IPMA open API wind-speed-daily-classe.json."""
     def __init__(self, data):
         super().__init__(self.__class__.__name__, data, 'classWindSpeed')
-    
+
     @classmethod
     async def get(cls, api):
         raw = await api.retrieve(API_WIND_SPEED_DAILY)
 
         return cls(raw)
 
-class Precipitation(Entities):
+class Precipitation(Entities): # pylint: disable=too-many-ancestors
+    """Object based representation of IPMA open API precipitation_classe.json."""
     def __init__(self, data):
         super().__init__(self.__class__.__name__, data, 'classPrecInt')
-    
+
     @classmethod
     async def get(cls, api):
         raw = await api.retrieve(API_PRECIPITATION)
 
         return cls(raw)
-
-
